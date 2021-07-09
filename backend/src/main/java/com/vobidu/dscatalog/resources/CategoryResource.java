55da915eee9787746c9a77f9ada679ef64f37724
Matheus Gois <mrochagois@gmail.com>
2021-07-08 23:15:39 -0300

    DTO

--- backend/src/main/java/com/vobidu/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/vobidu/dscatalog/resources/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.vobidu.dscatalog.entities.Category;
+import com.vobidu.dscatalog.dto.CategoryDTO;
 import com.vobidu.dscatalog.services.CategoryService;
 
 @RestController
@@ -19,8 +19,8 @@ public class CategoryResource {
 	CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
--- backend/src/main/java/com/vobidu/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/vobidu/dscatalog/services/CategoryService.java
@@ -1,10 +1,12 @@
 package com.vobidu.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.vobidu.dscatalog.dto.CategoryDTO;
 import com.vobidu.dscatalog.entities.Category;
 import com.vobidu.dscatalog.repositories.CategoryRepository;
 
@@ -14,7 +16,11 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		List<CategoryDTO> listDto = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
+		return listDto;
 	}
 }
