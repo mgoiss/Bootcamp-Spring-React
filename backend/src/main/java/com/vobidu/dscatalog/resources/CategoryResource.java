7fefa3ee2a96a8bf110989607ee69e71895d62c8
Matheus Gois <mrochagois@gmail.com>
2021-07-20 22:53:53 -0300

    POST new Category

--- backend/src/main/java/com/vobidu/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/vobidu/dscatalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.vobidu.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vobidu.dscatalog.dto.CategoryDTO;
 import com.vobidu.dscatalog.services.CategoryService;
@@ -30,5 +34,14 @@ public class CategoryResource {
 	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id ){
 		CategoryDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+		dto = service.insert(dto);
+		//Retornando o local onde foi feita a inserção
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);		
 	}
 }
